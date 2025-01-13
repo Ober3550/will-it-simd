ac2b24dd917682aca56fd7f3858c96897389d7bb
Oliver Mainey <oliver.mainey@dominos.com.au>
2025-01-13 18:57:56 +1000

    Updated validation and performance tests to be general purpose

--- validation_tests.sh
+++ validation_tests.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/env bash
 
-# Build all apps
-./matmul/c/build.sh
+dir_list="$(ls -d matmul/*)"
+# shellcheck disable=SC2206
+targets=(${dir_list[@]/'matmul/test'/})
 
-# Run against test data
-./matmul/c/run.sh ../test/matrix_a.csv ../test/matrix_b.csv ../test/result.csv
-
-# Compare result with expected
-cmp ./matmul/test/expected_result.csv ./matmul/test/result.csv && echo "Result and Expected Match"
+for target in "${targets[@]}"; do
+  if [[ -f "$target/build.sh" ]]; then
+    echo "Building $target"
+    "$target/build.sh"
+  fi
+  if [[ -f "$target/run.sh" ]]; then
+    echo "Running $target"
+    "$target/run.sh" ../test/matrix_a.csv ../test/matrix_b.csv ../test/result.csv
+    cmp "./matmul/test/expected_result.csv" "./matmul/test/result.csv" && echo "Result Matrix Matches Expected"
+    echo
+  fi
+done
