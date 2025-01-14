cc49369ce2d3303383a8173e44e9ddf2e7182981
Oliver Mainey <oliver.mainey@dominos.com.au>
2025-01-14 18:49:31 +1000

    Fixed zig performance test with allocation problem

--- matmul/zig/matmul.zig
+++ matmul/zig/matmul.zig
@@ -6,10 +6,11 @@ const Matrix = struct {
     data: []f32,
 };
 
-pub fn matrix_multiply(matrix_a: Matrix, matrix_b: Matrix, allocator: *const std.mem.Allocator) !Matrix {
+pub fn matrix_multiply(matrix_a: Matrix, matrix_b: Matrix) !Matrix {
     if (matrix_a.dimensions[1] != matrix_b.dimensions[0]) {
         return MatmulError.IncorrectDimensions;
     }
+    const allocator = std.heap.c_allocator;
     const result_dimensions: [2]u32 = [_]u32{ matrix_a.dimensions[0], matrix_b.dimensions[1] };
     std.debug.print("Allocating result matrix with dimensions: {d}, {d}\n", .{ result_dimensions[0], result_dimensions[1] });
     var result_matrix = try allocator.alloc(f32, result_dimensions[0] * result_dimensions[1]);
@@ -42,37 +43,77 @@ pub fn trim(str: []const u8) []const u8 {
     return str[start..end];
 }
 
-pub fn read_matrix(filename: []const u8, allocator: *const std.mem.Allocator) !Matrix {
+pub fn read_matrix(filename: []const u8) !Matrix {
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-
-    var buf: [4096]u8 = undefined;
-    var first_line: bool = true;
-    var dimensions: [2]u32 = undefined;
-    var matrix: []f32 = undefined;
+    var num: [30]u8 = undefined;
+    var num_len: u64 = 0;
+    const num_cap: u64 = 30;
     var row: u32 = 0;
     var col: u32 = 0;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var iterator = std.mem.tokenizeScalar(u8, line, ',');
-        if (first_line) {
-            const dim_x = trim(iterator.next().?);
-            const dim_y = trim(iterator.next().?);
-            dimensions[0] = try std.fmt.parseInt(u32, dim_x, 0);
-            dimensions[1] = try std.fmt.parseInt(u32, dim_y, 0);
-            matrix = try allocator.alloc(f32, dimensions[0] * dimensions[1]);
-            first_line = false;
+    var first_row = true;
+    var dimensions: [2]u32 = undefined;
+    var matrix: []f32 = undefined;
+    while (true) {
+        const byte = file.reader().readByte() catch break;
+        if (byte == '\r') {
             continue;
+        } else if (byte == '\n' or byte == ',') {
+            // std.debug.print("Col: {d} Row: {d} Num: {s}\n", .{ col, row, num[0..num_len] });
+            if (first_row) {
+                dimensions[col] = try std.fmt.parseInt(u32, trim(num[0..num_len]), 0);
+                if (col == 1) {
+                    std.debug.print("Allocating matrix with dimensions: {d}, {d}\n", .{ dimensions[0], dimensions[1] });
+                    matrix = try std.heap.c_allocator.alloc(f32, dimensions[0] * dimensions[1]);
+                    first_row = false;
+                    col = 0;
+                }
+            } else {
+                matrix[row * dimensions[0] + col] = try std.fmt.parseFloat(f32, trim(num[0..num_len]));
+                // Increment row
+                if (byte == '\n') {
+                    col = 0;
+                    row += 1;
+                }
+            }
+            // Increment col
+            if (byte == ',') {
+                col += 1;
+            }
+
+            // Reset num buffer
+            for (num_len..num_cap) |i| {
+                num[i] = 0;
+            }
+            num_len = 0;
+        } else if (num_len < num_cap) {
+            num[num_len] = byte;
+            num_len += 1;
         }
-        while (iterator.next()) |value| {
-            matrix[row * dimensions[0] + col] = try std.fmt.parseFloat(f32, value);
-            col += 1;
-        }
-        col = 0;
-        row += 1;
     }
+
+    // var first_line: bool = true;
+    // var row: u32 = 0;
+    // var col: u32 = 0;
+    // const allocator = std.heap.c_allocator;
+    // while (try file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', 2 ^ 20)) |line| {
+    //     var iterator = std.mem.tokenizeScalar(u8, line, ',');
+    //     if (first_line) {
+    //         const dim_x = trim(iterator.next().?);
+    //         const dim_y = trim(iterator.next().?);
+    //         dimensions[0] = try std.fmt.parseInt(u32, dim_x, 0);
+    //         dimensions[1] = try std.fmt.parseInt(u32, dim_y, 0);
+    //         matrix = try allocator.alloc(f32, dimensions[0] * dimensions[1]);
+    //         first_line = false;
+    //         continue;
+    //     }
+    //     while (iterator.next()) |value| {
+    //         matrix[row * dimensions[0] + col] = try std.fmt.parseFloat(f32, value);
+    //         col += 1;
+    //     }
+    //     col = 0;
+    //     row += 1;
+    // }
     return Matrix{ .dimensions = dimensions, .data = matrix };
 }
 
@@ -112,13 +153,9 @@ pub fn printMatrix(matrix: Matrix) void {
 }
 
 pub fn main() !void {
-    // Initialize an allocator
-    // Can be any one of them, this is the most basic one
-    const allocator = std.heap.c_allocator;
-
     // Initialize arguments
     // Then deinitialize at the end of scope
-    var argsIterator = try std.process.ArgIterator.initWithAllocator(allocator);
+    var argsIterator = try std.process.ArgIterator.initWithAllocator(std.heap.c_allocator);
     defer argsIterator.deinit();
 
     // Skip executable
@@ -127,14 +164,14 @@ pub fn main() !void {
     var matrix_a: Matrix = undefined;
     var matrix_b: Matrix = undefined;
     if (argsIterator.next()) |filename_a| {
-        matrix_a = try read_matrix(filename_a, &allocator);
+        matrix_a = try read_matrix(filename_a);
     }
     if (argsIterator.next()) |filename_b| {
-        matrix_b = try read_matrix(filename_b, &allocator);
+        matrix_b = try read_matrix(filename_b);
     }
     // printMatrix(matrix_a);
     // printMatrix(matrix_b);
-    const matrix_result = try matrix_multiply(matrix_a, matrix_b, &allocator);
+    const matrix_result = try matrix_multiply(matrix_a, matrix_b);
     // printMatrix(matrix_result);
     if (argsIterator.next()) |filename_result| {
         try writeMatrix(matrix_result, filename_result);
